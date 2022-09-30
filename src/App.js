ce2555eaf36bbb53afe9c8d5f060ce66f37f3f79
Cynthia Castillo <ca.castillo@gmail.com>
2022-09-29 21:38:19 -0500

    Props, Eventos y State

--- src/App.js
+++ src/App.js
@@ -10,14 +10,19 @@ Detener el proyecto: Ctrl+C
 class App extends React.Component {
 
   render() {
+
+    /* AplicacionSegundaPalabra -> PascalCase
+       onClick -> camelCase
+    */
+
     return(
       <div className="container">
         <h1>Mi primera Aplicación con React</h1>
         <h2>wuju!</h2>
         <MiPrimerComponente />
-        <PersonCard />
-        <PersonCard />
-        <PersonCard />
+        <PersonCard firstName="Elena" lastName="De Troya" age={30} city="Roma" />
+        <PersonCard firstName="Juana" lastName="De Troya" age={20} city="México" />
+        <PersonCard firstName="Pedro" lastName="Páramo" age={35} city="Colombia" />
       </div>
     );
   }
--- src/componentes/PersonCard/PersonCard.js
+++ src/componentes/PersonCard/PersonCard.js
@@ -2,12 +2,47 @@ import React from "react";
 
 class PersonCard extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            edoCivil: "Soltero",
+            age: this.props.age
+        }
+    }
+
+    hizoClic = () => alert("Hiciste clic en función");
+
+    hizoClicEn = (e, firstName) => alert("Hiciste clic en:"+firstName);
+
+    funcionTradicional() {
+        alert("Esta es una alerta");
+    }
+
+    cambiarEdoCivil = () => {
+        if (this.state.edoCivil === "Soltero"){
+            this.setState({edoCivil: "Casado"})
+        }else {
+            this.setState({edoCivil: "Soltero"})
+        }
+    }
+
+    cumpleaños = () => {
+        this.setState({age: this.state.age+1})
+    }
+
     render() {
+
+        const {firstName, lastName, city} = this.props;
+
         return(
             <div className="card">
-                <h4>Elena de Troya</h4>
-                <p><b>Edad:</b> 30</p>
-                <p><b>Ciudad:</b> Roma</p>
+                <h4>{firstName} {lastName}</h4>
+                <p><b>Edad:</b> {this.state.age}</p>
+                <p><b>Ciudad:</b> {city}</p>
+                <p><b>Estado Civil:</b>{this.state.edoCivil}</p>
+                <button className="btn btn-success" onClick={this.cambiarEdoCivil}>Cambiar Estado Civil</button>
+                <button className="btn btn-warning" onClick={ (e)=>this.hizoClicEn(e, firstName) }>Haz Clic aquí</button>
+                <button className="btn btn-info" onClick={this.cumpleaños}>Botón de Cumpleaños {firstName}</button>
             </div>
         )
     }

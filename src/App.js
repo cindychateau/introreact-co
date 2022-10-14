d3f33a6f10c805c256ef6dea9ec348ab79657821
Cynthia Castillo <ca.castillo@gmail.com>
2022-10-13 20:43:29 -0500

    Router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import MiPrimerComponenteFuncional from "./componentes/MiPrimerComponenteFuncion
 import PersonCard from "./componentes/PersonCard/PersonCard";
 import PersonCardFuncional from "./componentes/PersonCardFuncional/PersonCardFuncional";
 
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
 /*
 Inicializar proyecto: npm start
 Detener el proyecto: Ctrl+C
@@ -51,17 +53,36 @@ class App extends React.Component {
         <h1>Mi primera Aplicación con React</h1>
         <h2>wuju!</h2>
 
-        <MiPrimerComponente />
+        <BrowserRouter>
 
-        <MiPrimerComponenteFuncional />
+          <Link to="/" className="btn btn-success">Home</Link>
+          <Link to="/funcional" className="btn btn-info">Componente Funcional</Link>
+          <Link to="/formulario" className="btn btn-primary">Formulario</Link>
+          <Link to="/api" className="btn btn-warning">Componente API</Link>
+          <Link to="/apiaxios" className="btn btn-danger">Componente Axios</Link>
 
-        <PersonCard firstName="Elena" lastName="De Troya" age={30} city="Roma" />
+          <Switch>
+            <Route path="/" exact render={() => <MiPrimerComponente />} />
+            <Route path="/funcional" exact render={() => <MiPrimerComponenteFuncional />} />
+            <Route path="/funcional/:texto" exact render={(routeProps) => <MiPrimerComponenteFuncional {...routeProps} />} />
+            <Route path="/formulario" render={() => <Formulario />} />
+            <Route path="/api" render={() => <ComponenteApi />} />
+            <Route path="/apiaxios" render={() => <ComponenteAxios />} />
+          </Switch>
+
+        </BrowserRouter>
+
+        
+
+        
+
+        {/* <PersonCard firstName="Elena" lastName="De Troya" age={30} city="Roma" />
 
         <PersonCard firstName="Juana" lastName="De Troya" age={20} city="México" />
 
         <PersonCard firstName="Pedro" lastName="Páramo" age={35} city="Colombia" />
 
-        <PersonCardFuncional firstName="Pablo" lastName="Picasso" age={50} city="Medellin" />
+        <PersonCardFuncional firstName="Pablo" lastName="Picasso" age={50} city="Medellin" /> */}
 
         {
 
@@ -69,13 +90,7 @@ class App extends React.Component {
           //   <PersonCardFuncional firstName={persona.firstName} lastName={persona.lastName} age={persona.age} city={persona.city} />
           // )
 
-        }        
-
-        <Formulario />
-
-        <ComponenteApi />
-
-        <ComponenteAxios />
+        }    
 
       </div>
     );
--- src/componentes/MiPrimerComponenteFuncional/MiPrimerComponenteFuncional.js
+++ src/componentes/MiPrimerComponenteFuncional/MiPrimerComponenteFuncional.js
@@ -1,8 +1,12 @@
-const MiPrimerComponenteFuncional = () => {
+import { useParams } from 'react-router-dom';
+
+const MiPrimerComponenteFuncional = props => {
+
+    const {texto} = useParams();
 
     return(
         <div>
-            <h2>Este es mi primer componente funcional</h2>
+            <h2>Este es mi primer componente funcional {texto} </h2>
         </div>
     )
 
